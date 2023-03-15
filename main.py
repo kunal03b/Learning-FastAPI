83a93790fcf13faa7d2a15411f9d986f95fd2fa0
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-15 12:13:10 +0530

    Id fixed

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from typing import List
-from uuid import uuid4
+from uuid import uuid4,UUID
 from fastapi import FastAPI
 from models import User,Gender,Role
 
 app = FastAPI()
 
 db: List[User] = [
-    User(id=uuid4(),first_name="Kunal",
+    User(id=UUID("fe622abc-15a3-4476-8faa-a8b8a99d68e5"),
+         first_name="Kunal",
          last_name="Sharma",
          Gender=Gender.male,
          roles = [Role.admin,Role.user]
          ),
-    User(id=uuid4(),first_name="Sushant",
+    User(id=UUID("0d5e3b16-ad53-41e4-aad4-f62f5549804a"),first_name="Sushant",
          last_name="Dhiman",
          Gender=Gender.male,
          roles = [Role.student]
@@ -22,3 +23,12 @@ db: List[User] = [
 @app.get("/")
 async def read_root():
     return {"Hello": "Kunal"}
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db; 
+
+@app.post("/api/v1/users")
+async def register_user(user:User):
+    db.append(user)
+    return{"id":user.id}
