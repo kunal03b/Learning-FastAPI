ac2e1b813e14c252822466d9144aa350db1394bb
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-15 11:39:00 +0530

    .

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+from typing import List
+from uuid import uuid4
 from fastapi import FastAPI
+from models import User,Gender,Role
 
 app = FastAPI()
+
+db: List[User] = [
+    User(id=uuid4(),first_name="Kunal",
+         last_name="Sharma",
+         Gender=Gender.male,
+         roles = [Role.admin,Role.user]
+         ),
+    User(id=uuid4(),first_name="Sushant",
+         last_name="Dhiman",
+         Gender=Gender.male,
+         roles = [Role.student]
+         )     
+]
 
 
 @app.get("/")
