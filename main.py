5a82f4d5d39bc1fd1a6dcdded68150fa066bdf11
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-17 21:54:58 +0530

    Delete functionality added

--- main.py
+++ main.py
@@ -32,3 +32,10 @@ async def fetch_users():
 async def register_user(user:User):
     db.append(user)
     return{"id":user.id}
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return
