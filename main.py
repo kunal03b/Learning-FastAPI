ebe0c10d84fe247dbba5f2013acd4754e4aa019c
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-17 21:59:26 +0530

    404 Error added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 from uuid import uuid4,UUID
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from models import User,Gender,Role
 
 app = FastAPI()
@@ -39,3 +39,7 @@ async def delete_user(user_id: UUID):
         if user.id == user_id:
             db.remove(user)
             return
+    raise HTTPException(
+        status_code=404,
+        detail = f"user with id:{user_id} does not exists"
+    )
